d4d90a0dfca327e61011c36fa309bc44582cf270
AlessandroMuscio <muscioalex30@gmail.com>
2024-05-14 12:30:20 +0200

    refactor: cleaned repo for better refcatoring

--- src/main/java/com/raven/datechooser/DateChooserException.java
+++
@@ -1,8 +0,0 @@
-package com.raven.datechooser;
-
-public class DateChooserException extends RuntimeException {
-
-    public DateChooserException(String errorMessage) {
-        super(errorMessage);
-    }
-}
--- src/main/java/com/raven/datechooser/DateSelectable.java
+++
@@ -1,8 +0,0 @@
-package com.raven.datechooser;
-
-import java.util.Date;
-
-public interface DateSelectable {
-
-    public boolean isDateSelectable(Date date);
-}
