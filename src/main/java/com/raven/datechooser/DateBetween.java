8e99c8cbc0f5ffbbbde6a2ca3aa7ea5f24c2139a
AlessandroMuscio <muscioalex30@gmail.com>
2024-05-14 16:51:10 +0200

    fix: better code removed the need for the exception

--- src/main/java/com/raven/datechooser/DateBetween.java
+++ src/main/java/com/raven/datechooser/DateBetween.java
@@ -2,17 +2,18 @@ package com.raven.datechooser;
 
 import java.time.LocalDate;
 
-import com.raven.datechooser.error.DatesFlippedException;
-
 public class DateBetween {
   public static final String DATES_FLIPPED_ERROR = "The given dates are flipped!\nThe `from` date can't be after the `to` date.";
 
   private final LocalDate from;
   private final LocalDate to;
 
-  public DateBetween(LocalDate from, LocalDate to) throws DatesFlippedException {
-    if (from.isAfter(to))
-      throw new DatesFlippedException(DateBetween.DATES_FLIPPED_ERROR);
+  public DateBetween(LocalDate from, LocalDate to) {
+    if (from.isAfter(to)) {
+      LocalDate tmp = from;
+      from = to;
+      to = tmp;
+    }
 
     this.from = from;
     this.to = to;
